185b7107c5ec2aa8301a9fd2193e07f512adf01b
dahc14 <dahc14@student.bth.se>
2015-04-22 11:43:40 -0500

    Signed-off-by: dahc14 <dahc14@student.bth.se>

--- test/config.php
+++ test/config.php
@@ -1,7 +1,3 @@
 <?php
 	include __DIR__ . "/../autoloader.php";
-	include __DIR__ . "/../vendor/autoload.php";
-	
-	define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../vendor/anax/mvc') . '/');
-	define('ANAX_APP_PATH', ANAX_INSTALL_PATH . 'app/');
 ?>
